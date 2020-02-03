002721e96c9144b5f3c9e370f8af93582162e53c
Alex Wilson <wilsonc.alexander@gmail.com>
2020-02-03 06:52:26 -0500

    rename preset

--- app/aem/src/server/options.ts
+++ app/aem/src/server/options.ts
@@ -3,6 +3,6 @@ const packageJson = require('../../package.json');
 
 export default {
   packageJson,
-  framework: 'html',
-  frameworkPresets: [require.resolve('./framework-preset-html.js')],
+  framework: 'aem',
+  frameworkPresets: [require.resolve('./framework-preset-aem.js')],
 };
