71f07c1240b1112a75f904a0464f7f310a895244
Alex Wilson <wilsonc.alexander@gmail.com>
2020-02-19 05:54:46 -0500

    remove copyright that has been moved to license

--- htl-loader/.eslintrc.js
+++ htl-loader/.eslintrc.js
@@ -1,15 +1,3 @@
-/*
- * Copyright 2019 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-
 module.exports = {
   'env': {
     'node': true,
@@ -32,19 +20,5 @@ module.exports = {
 
     // allow dangling underscores for 'fields'
     'no-underscore-dangle': ['error', {'allowAfterThis': true}],
-
-    // enforce license header
-    'header/header': [2, 'block', ['',
-      { pattern: ' * Copyright \\d{4} .*\\. All rights reserved\\.', template: ' * Copyright 2019 Adobe. All rights reserved.'},
-      ' * This file is licensed to you under the Apache License, Version 2.0 (the "License");',
-      ' * you may not use this file except in compliance with the License. You may obtain a copy',
-      ' * of the License at http://www.apache.org/licenses/LICENSE-2.0',
-      ' *',
-      ' * Unless required by applicable law or agreed to in writing, software distributed under',
-      ' * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS',
-      ' * OF ANY KIND, either express or implied. See the License for the specific language',
-      ' * governing permissions and limitations under the License.',
-      ' ',
-    ]]
   }
 };
