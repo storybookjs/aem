74b7654f16af73386d028ba4dc62bdec3f4b6f16
Alex Wilson <wilsonc.alexander@gmail.com>
2020-03-09 21:00:44 -0400

    convert source from xml to json and pull data in for the component loader

--- app/aem/src/server/component-loader.js
+++ app/aem/src/server/component-loader.js
@@ -1,15 +1,17 @@
 const path = require('path');
+const parser = require('xml2json');
 
 module.exports = async function(source) {
   const resourceType = this.context.substring(this.rootContext.length + 1);
+  const json = JSON.parse(parser.toJson(source));
   const name = path.basename(this.context);
   const component = {
-    // todo: parse XML from `source`
     resourceType,
     properties: {
-      'jcr:title': `${name}`,
+      'jcr:title': `${json['jcr:root'] ? json['jcr:root']['jcr:title'] : name}`,
     }
   };
+
   const code = [
     `const component = ${JSON.stringify(component)};`,
     `component.module = require('${this.context}/${name}.html');`,
