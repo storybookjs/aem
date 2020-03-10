6832711f282fa36c593008e534e3325311ba9a2d
Alex Wilson <wilsonc.alexander@gmail.com>
2020-03-10 00:51:55 -0400

    switched around the ordering of the functions

--- app/aem/src/server/aem-component-loader.js
+++ app/aem/src/server/aem-component-loader.js
@@ -4,6 +4,20 @@ const parser = require('xml2json');
 const JCR_ROOT_KEY = 'jcr:root';
 const JCR_TITLE_KEY = 'jcr:title';
 
+const getRequiredHTL = (component, context, pathBaseName) => {
+  return `const component = ${JSON.stringify(component)};
+   component.module = require('${context}/${pathBaseName}.html');
+   module.exports = component;`;
+}
+
+const getRequiredClientLibs = (componentDir) => {
+  const requireStatements = [];
+  glob.sync(`${componentDir}/**/clientlibs/**/{css,less,js}/*`).forEach((file)=> {
+    requireStatements.push(`require('${path.resolve(file)}')`)
+  })
+  return requireStatements;
+}
+
 module.exports = async function(source) {
   const resourceType = this.context.substring(this.rootContext.length + 1);
   const json = JSON.parse(parser.toJson(source));
@@ -21,17 +35,3 @@ module.exports = async function(source) {
   code.push(getRequiredHTL(component, this.context, pathBaseName));
   return code.join('\n');
 };
-
-const getRequiredHTL = (component, context, pathBaseName) => {
-  return `const component = ${JSON.stringify(component)};
-   component.module = require('${context}/${pathBaseName}.html');
-   module.exports = component;`;
-}
-
-const getRequiredClientLibs = (componentDir) => {
-  const requireStatements = [];
-  glob.sync(`${componentDir}/**/clientlibs/**/{css,less,js}/*`).forEach((file)=> {
-    requireStatements.push(`require('${path.resolve(file)}')`)
-  })
-  return requireStatements;
-}
