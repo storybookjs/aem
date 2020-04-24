c8d5bc470c1e7237cc5ae6bcc2d29eb27accf5b9
Tobias Bocanegra <tripod@adobe.com>
2020-04-24 10:19:07 +0900

    fix(runtime): add global properties
    fixes #90

---
+++ examples/aem-kitchen-sink/components/globals/gobals.stories.js
@@ -0,0 +1,19 @@
+import { withKnobs, text } from "@storybook/addon-knobs";
+import { aemMetadata } from '@storybook/aem';
+import Template from './globals.html';
+
+export default {
+  title: 'Globals',
+  decorators: [
+    withKnobs,
+  ],
+};
+
+export const Globals = () => {
+  return {
+    content: {
+      text: text('text', 'Hello, world.' ),
+    },
+    template: Template,
+  };
+};
