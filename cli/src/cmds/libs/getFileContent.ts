e126f3579fd3a6a250ec364ab8a792fee2dc9fe5
jzeltman <joshua.zeltman@gmail.com>
2020-04-28 14:59:09 -0300

    changed arrangement of files

--- cli/src/cmds/libs/getFileContent.ts
+++ cli/src/cmds/libs/getFileContent.ts
@@ -6,18 +6,32 @@ export const getFileContent = async (fileArray, componentTypes) => {
   if (!fileArray || fileArray.length === 0) throw new Error(`No files found`);
 
   const content = {};
-  /* eslint-disable no-return-assign */
-  componentTypes.forEach(type => (content[type] = {}));
+  componentTypes.forEach(type => {
+    content[type] = {
+      html: {},
+      entry: ``,
+      js: ``,
+      css: ``,
+    };
+  });
 
   /* eslint-disable no-plusplus, no-await-in-loop */
   for (let i = 0; i < fileArray.length; i++) {
     const { path, name } = fileArray[i];
     const rawContent = await fetchFromAEM({ url: path });
+    const minifiedHTL = minifyHTL(await rawContent.text());
     const [componentType] = path.split(basename(path));
+    let componentName = componentType.slice(0, -1).split('/');
+    componentName = componentName[componentName.length - 1];
+
     if (content[componentType]) {
-      content[componentType][name] = minifyHTL(await rawContent.text());
+      if (basename(name, '.html') === componentName) {
+        content[componentType].entry = minifiedHTL;
+      } else {
+        content[componentType].html[name] = minifiedHTL;
+      }
     } else {
-      content[path] = minifyHTL(await rawContent.text());
+      content[path] = minifiedHTL;
     }
   }
 
