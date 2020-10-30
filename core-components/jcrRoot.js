53ba5d6af6cc9ce4cab2aa08504a2d7ccce16d53
CodeByAlex <wilsonc.alexander@gmail.com>
2020-10-29 20:08:14 -0400

    added jcr roots

--- core-components/jcrRoot.js
+++ core-components/jcrRoot.js
@@ -1,2 +1,5 @@
 const path = require('path');
+AEMRegisterJcrRoot(path.resolve(__dirname, './apps'));
+AEMRegisterJcrRoot(path.resolve(__dirname, './core'));
+AEMRegisterJcrRoot(path.resolve(__dirname, './libs'));
 AEMRegisterJcrRoot(path.resolve(__dirname, '.'));
